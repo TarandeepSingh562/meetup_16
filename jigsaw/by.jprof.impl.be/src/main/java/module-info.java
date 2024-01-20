7415ac9d089e5bd03fad498c2c8d1fac88fd72e6
Tarandeep Singh <tarandeep.singh562@gmail.com>
2024-01-19 17:49:45 -0800

    jdk-inconsistencies

--- jigsaw/by.jprof.impl.be/src/main/java/module-info.java
+++ jigsaw/by.jprof.impl.be/src/main/java/module-info.java
@@ -1,5 +1,4 @@
 module by.jprof.impl.be {
 	requires by.jprof.api;
-	exports by.jprof.impl.be;
 	provides by.jprof.api.GreetingsService with by.jprof.impl.be.GreetingsServiceBe;
 }
--- jigsaw/by.jprof.impl.chr/src/main/java/module-info.java
+++ jigsaw/by.jprof.impl.chr/src/main/java/module-info.java
@@ -1,5 +1,4 @@
 module by.jprof.impl.chr {
 	requires by.jprof.api;
-	exports by.jprof.impl.chr;
 	provides by.jprof.api.GreetingsService with by.jprof.impl.chr.GreetingsServiceChr;
 }
